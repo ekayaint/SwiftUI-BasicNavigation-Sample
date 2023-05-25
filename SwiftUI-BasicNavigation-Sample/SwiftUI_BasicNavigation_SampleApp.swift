0faec2b584555cdbf7d93254ea2920db2c55d196
ekayaint <ekayaint@gmail.com>
2023-05-25 21:09:11 +0300

    Initial Commit

---
+++ SwiftUI-BasicNavigation-Sample/SwiftUI_BasicNavigation_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BasicNavigation_SampleApp.swift
+//  SwiftUI-BasicNavigation-Sample
+//
+//  Created by ekayaint on 25.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BasicNavigation_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
